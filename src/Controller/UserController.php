d2b01e8203aca5195c77977de9a211f9c8dd6a2a
Christian Baltazar <ing.chris@hotmail.com>
2025-07-09 23:31:31 +0200

    downgrade docparser and create user form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Cassandra\Type\UserType;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,6 +34,34 @@ class UserController extends AbstractController
     {
         return $this->render('user/show.html.twig', [
             'user' => $user,
+        ]);
+    }
+
+    #[Route('/user/edit', name: 'app_user_edit', methods: ['GET', 'POST'], priority: 2)]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+//        $form = $this->createForm(UserType::class, new User());
+        $form = $this->createFormBuilder(new User())
+            ->add('name')
+            ->add('email')
+            ->add('phone')
+            ->add('Save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+            $user->setCreatedAt(new DateTimeImmutable());
+            $user->setUpdatedAt(new DateTimeImmutable());
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('user/form.html.twig', [
+            'form' => $form,
         ]);
     }
 }
