aa5389abb7743998884b193d1ca4dab0d11bfa89
Christian Baltazar <ing.chris@hotmail.com>
2025-07-12 11:11:50 +0200

    Logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -18,4 +18,10 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Handled by symfony
+    }
 }
