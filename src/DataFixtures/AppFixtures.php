f6e17c2810fad01d9f427327ae8dfe81dd97e4ee
Christian Baltazar <ing.chris@hotmail.com>
2025-07-13 16:39:06 +0200

    Fixture 2 users and fix menu

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,13 +15,21 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $user = new User();
-        $user->setEmail('foo@test.com');
-        $user->setName('John Doe');
-        $user->setPhone('0123456789');
-        $user->setPassword($this->hasher->hashPassword($user, '123'));
+        $user1 = new User();
+        $user1->setEmail('foo@test.com');
+        $user1->setName('John Doe');
+        $user1->setPhone('0123456789');
+        $user1->setRoles(['ROLE_ADMIN']);
+        $user1->setPassword($this->hasher->hashPassword($user1, '123'));
 
-        $manager->persist($user);
+        $user2 = new User();
+        $user2->setEmail('bar@test.com');
+        $user2->setName('Jane Doe');
+        $user2->setPhone('0123456789');
+        $user2->setPassword($this->hasher->hashPassword($user1, '123'));
+
+        $manager->persist($user1);
+        $manager->persist($user2);
         $manager->flush();
     }
 }
