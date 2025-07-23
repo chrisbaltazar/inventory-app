bd073f9b001a10de81ad81ec0151881c6cba86bd
Christian Baltazar <ing.chris@hotmail.com>
2025-07-23 19:34:24 +0200

    chore(test_warmup): create

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Kernel;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\Filesystem\Filesystem;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -17,33 +12,4 @@ if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
 
-$kernel = new Kernel('test', true);
-$kernel->boot();
-$container = $kernel->getContainer();
-$application = new Application($kernel);
-$application->setAutoExit(false);
-
-$fs = new Filesystem();
-$databasePath = $container->getParameter('kernel.project_dir') . '/var/data_test.db';
-if (!$fs->exists($databasePath)) {
-    // Create database
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:create',
-        '--env' => 'test',
-    ]), new NullOutput());
-
-    // Create schema
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:schema:create',
-        '--env' => 'test',
-    ]), new NullOutput());
-
-    // Load fixtures
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:fixtures:load',
-        '--env' => 'test',
-        '--no-interaction' => true,
-    ]), new NullOutput());
-}
-
-$kernel->shutdown();
+require_once 'test_warmup.php';
---
+++ tests/test_warmup.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Filesystem\Filesystem;
+
+$kernel = new Kernel('test', true);
+$kernel->boot();
+$container = $kernel->getContainer();
+$application = new Application($kernel);
+$application->setAutoExit(false);
+
+$fs = new Filesystem();
+$databasePath = $container->getParameter('kernel.project_dir') . '/var/data_test.db';
+if (!$fs->exists($databasePath)) {
+    // Create database
+    $application->run(new ArrayInput([
+        'command' => 'doctrine:database:create',
+        '--env' => 'test',
+    ]), new NullOutput());
+
+    // Create schema
+    $application->run(new ArrayInput([
+        'command' => 'doctrine:schema:create',
+        '--env' => 'test',
+    ]), new NullOutput());
+
+    // Load fixtures
+    $application->run(new ArrayInput([
+        'command' => 'doctrine:fixtures:load',
+        '--env' => 'test',
+        '--no-interaction' => true,
+    ]), new NullOutput());
+}
+
+$kernel->shutdown();
