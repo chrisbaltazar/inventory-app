b9e642d179a65a2d72a1bf2da1361997b3d0d36d
Christian Baltazar <ing.chris@hotmail.com>
2025-07-23 19:54:33 +0200

    chore(menu): improve route logic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("IS_AUTHENTICATED_FULLY")]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home_index')]
