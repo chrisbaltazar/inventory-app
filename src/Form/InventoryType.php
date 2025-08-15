af31cd7451d6bb73720c86c24f0e43c93cd98d80
Christian Baltazar <ing.chris@hotmail.com>
2025-08-15 10:58:37 +0200

    Show loan on item

--- src/Form/InventoryType.php
+++ src/Form/InventoryType.php
@@ -7,7 +7,6 @@ use App\Enum\SizeEnum;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
