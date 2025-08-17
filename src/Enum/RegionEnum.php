68db16f75adf052b8f6fb68f712b0e433ed2da47
Christian Baltazar <ing.chris@hotmail.com>
2025-08-17 19:53:41 +0200

    Complete region list

--- src/Enum/RegionEnum.php
+++ src/Enum/RegionEnum.php
@@ -6,9 +6,19 @@ enum RegionEnum: string
 {
     use WithEnumValues;
 
+    case ACCESORIOS = 'ACCESORIOS';
+    case ENSAYO = 'ENSAYO';
     case JALISCO = 'JALISCO';
     case HIDALGO = 'HIDALGO';
     case OAXACA = 'OAXACA';
-    case QUERETARO = 'QUERÉTARO';
     case VERACRUZ = 'VERACRÚZ';
+    case NAYARIT = 'NAYARIT';
+    case YUCATAN = 'YUCATÁN';
+    case CHIAPAS = 'CHIAPAS';
+    case NORTE = 'NORTE';
+    case MICHOACAN = 'MICHOACÁN';
+    case REVOLUCION = 'REVOLUCIÓN';
+    case GUERRERO = 'GUERRERO';
+    case PREHISPANICO = 'PREHISPÁNICO';
+    case BAJA_CALIFORNIA = 'BAJA CALIFORNIA';
 }
