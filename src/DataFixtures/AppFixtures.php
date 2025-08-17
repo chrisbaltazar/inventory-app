55b1001f8cf0c3a52cd677e45211f757a0271c6d
Christian Baltazar <ing.chris@hotmail.com>
2025-08-17 20:16:43 +0200

    Require fixtures bundle again

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,6 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
