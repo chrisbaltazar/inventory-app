30f4f51592deee7bdb2e649824417dedf9269081
Christian Baltazar <ing.chris@hotmail.com>
2025-09-05 21:31:03 +0200

    test(LoanController): store OK

--- tests/Application/Controller/LoanControllerTest.php
+++ tests/Application/Controller/LoanControllerTest.php
@@ -6,8 +6,6 @@ use App\DataFixtures\Factory\EventFactory;
 use App\DataFixtures\Factory\InventoryFactory;
 use App\DataFixtures\Factory\ItemFactory;
 use App\DataFixtures\Factory\UserFactory;
-use App\Entity\Inventory;
-use App\Entity\Item;
 use App\Enum\RegionEnum;
 use App\Tests\AbstractWebTestCase;
 use App\Tests\Trait\WithUserSession;
@@ -43,5 +41,42 @@ class LoanControllerTest extends AbstractWebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorCount(1, 'table.table tr');
         $this->assertSelectorCount(2, 'table.table select option');
+    }
+
+    public function testStoreLoanOK(): void
+    {
+        $user = UserFactory::admin();
+        $event = EventFactory::create();
+        $item1 = ItemFactory::create(RegionEnum::ACCESORIOS->value);
+        $item1->addInventory(InventoryFactory::create(quantity: 2));
+        $item2 = ItemFactory::create(RegionEnum::ENSAYO->value);
+        $item2->addInventory(InventoryFactory::create(quantity: 1));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->persist($event);
+        $this->entityManager->persist($item1);
+        $this->entityManager->persist($item2);
+        $this->entityManager->flush();
+
+        $this->asUser($this->client, $user)->request('POST', '/loan/store', [
+            'loan' => [
+                'event' => $event->getId(),
+                'user' => $user->getId(),
+                'item' => [
+                    sprintf(
+                        '%d|%s',
+                        $item1->getInventory()->first()->getId(),
+                        $item1->getInventory()->first()->getSize()
+                    ),
+                    sprintf(
+                        '%d|%s',
+                        $item2->getInventory()->first()->getId(),
+                        $item2->getInventory()->first()->getSize()
+                    ),
+                ],
+            ],
+        ]);
+
+        $this->assertResponseIsSuccessful();
     }
 }
