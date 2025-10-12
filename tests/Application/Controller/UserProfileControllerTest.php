1134f5a8a13602368e7da790d2caed0465496b94
Christian Baltazar <ing.chris@hotmail.com>
2025-10-12 13:41:40 +0200

    Test UserPasswordChanges

--- tests/Application/Controller/UserProfileControllerTest.php
+++ tests/Application/Controller/UserProfileControllerTest.php
@@ -38,7 +38,6 @@ class UserProfileControllerTest extends AbstractWebTestCase
         self::assertSame('+34123456789', $user->getPhone());
         self::assertSame('1990-01-01', $user->getBirthday()->format('Y-m-d'));
         self::assertTrue($user->isProfileComplete());
-
         self::assertResponseRedirects('/');
     }
 
@@ -53,7 +52,43 @@ class UserProfileControllerTest extends AbstractWebTestCase
         $this->asUser($this->client, $wolf)->request('GET', "/user/profile/{$lamb->getId()}");
 
         self::assertFalse($wolf->isAdmin());
+        self::assertResponseStatusCodeSame(403);
+    }
 
+    public function testUpdatePassword(): void
+    {
+        $user = UserFactory::create();
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+
+        $this->asUser($this->client, $user)->request('GET', "/user/{$user->getId()}/password");
+
+        $currentPwd = $user->getPassword();
+
+        $this->client->submitForm('Guardar', [
+            'user_password[plainPassword][first]' => 'fooPassword99',
+            'user_password[plainPassword][second]' => 'fooPassword99',
+        ]);
+
+        $user = $this->client->getContainer()->get(UserRepository::class)->find($user->getId());
+
+        self::assertNotEmpty($user->getPassword());
+        self::assertNotSame($currentPwd, $user->getPassword());
+        self::assertResponseRedirects('/user/profile');
+    }
+
+    public function testOtherPasswordUpdateDenied(): void
+    {
+        $lamb = UserFactory::create();
+        $wolf = UserFactory::create();
+        $this->entityManager->persist($lamb);
+        $this->entityManager->persist($wolf);
+        $this->entityManager->flush();
+
+        $this->asUser($this->client, $wolf)->request('GET', "/user/{$lamb->getId()}/password");
+
+        self::assertFalse($wolf->isAdmin());
         self::assertResponseStatusCodeSame(403);
     }
 }
