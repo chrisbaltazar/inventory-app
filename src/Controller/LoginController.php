cd81c23b4f6eacc167fe23bc93a7a0f4b8c4547a
Christian Baltazar <ing.chris@hotmail.com>
2025-10-18 11:52:20 +0200

    Implement google oauth

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Service\Auth\GoogleOAuthService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $utils): Response
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function index(AuthenticationUtils $utils, GoogleOAuthService $googleAuth): Response
     {
         if ($this->isGranted('ROLE_USER')) {
             return $this->redirectToRoute('app_home_index');
@@ -20,12 +25,37 @@ class LoginController extends AbstractController
 
         return $this->render('login/index.html.twig', [
             'error' => $error,
+            'googleAuthUrl' => $googleAuth->getAuthUrl(),
         ]);
     }
 
-    #[Route('/logout', name: 'app_logout')]
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout(): void
     {
         // Handled by symfony
+    }
+
+    #[Route('/auth', name: 'app_login_auth', methods: ['GET'])]
+    public function auth(
+        #[MapQueryParameter] string $code,
+        GoogleOAuthService $googleAuth,
+        UserRepository $repository,
+        Security $security,
+    ): Response {
+        try {
+            if (!$code) {
+                throw new \UnexpectedValueException('No code provided');
+            }
+            $googleAuth->setResponseCode($code);
+            $authUser = $googleAuth->getOAuthUser();
+            $user = $repository->findOneBy(['email' => $authUser->getEmail()]);
+            if (!$user) {
+                throw new \RuntimeException('User not found');
+            }
+
+            return $security->login($user);
+        } catch (\Throwable $t) {
+            throw $this->createAccessDeniedException();
+        }
     }
 }
--- src/Service/Auth/GoogleOAuthService.php
+++ src/Service/Auth/GoogleOAuthService.php
@@ -2,6 +2,54 @@
 
 namespace App\Service\Auth;
 
-class GoogleOAuthService {
+use Google\Client;
+use Google\Service\Oauth2;
+use Symfony\Component\Routing\RouterInterface;
 
+class GoogleOAuthService
+{
+    private string $accessToken;
+
+    public function __construct(
+        private readonly Client $client,
+        private readonly RouterInterface $router,
+    ) {
+        $this->configureClient($router);
+    }
+
+    private function configureClient(): void
+    {
+        $this->client->setRedirectUri($this->getRedirectUrl());
+        $this->client->addScope('email');
+        $this->client->addScope('profile');
+    }
+
+    public function getAuthUrl(): string
+    {
+        return $this->client->createAuthUrl();
+    }
+
+    public function getRedirectUrl(): string
+    {
+        return $this->router->generate(name: 'app_login_auth', referenceType: RouterInterface::ABSOLUTE_URL);
+    }
+
+    public function setResponseCode(string $code): void
+    {
+        $this->accessToken = $this->client->fetchAccessTokenWithAuthCode($code)['access_token'] ??
+            throw new \RuntimeException('Failed to fetch access token');
+
+        $this->client->setAccessToken($this->accessToken);
+    }
+
+    public function getOAuthUser(): Oauth2\Userinfo
+    {
+        if (!isset($this->accessToken)) {
+            throw new \BadMethodCallException('Access token is not set');
+        }
+
+        $auth = new Oauth2($this->client);
+
+        return $auth->userinfo->get();
+    }
 }
