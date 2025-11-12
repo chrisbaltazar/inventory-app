7a9227f7fab9bac86b75095b2c9097a7e739c293
Christian Baltazar <ing.chris@hotmail.com>
2025-11-12 15:41:03 +0100

    test3(LoginController): fix test

--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Controller;
 
 use App\DataFixtures\Factory\UserFactory;
+use App\Entity\User;
 use App\Service\Message\Channel\Sms\SMSProviderInterface;
-use tests\AbstractWebTestCase;
+use Tests\AbstractWebTestCase;
 
 class LoginControllerTest extends AbstractWebTestCase
 {
@@ -12,26 +13,30 @@ class LoginControllerTest extends AbstractWebTestCase
     {
         parent::setUp();
         $this->refreshDatabase();
+        $this->client->disableReboot();
     }
 
     public function testRecoverAccessValid()
     {
-        $this->markTestSkipped('Not working yet');
-
         $user = UserFactory::create();
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
         $smsProvider = $this->createMock(SMSProviderInterface::class);
         $smsProvider->expects($this->once())->method('send');
+        $userRepository = $this->entityManager->getRepository($user::class);
 
-        static::getContainer()->set(SMSProviderInterface::class, $smsProvider);
-//        $this->client->getContainer()->set(SMSProviderInterface::class, $smsProvider);
+        $this->client->getContainer()->set(SMSProviderInterface::class, $smsProvider);
         $this->client->request('GET', '/recover-access');
         $this->client->submitForm('Enviar', [
-            'recover_password[email]' => $user->getEmail()
+            'recover_password[email]' => $user->getEmail(),
         ]);
 
+        /** @var User $user */
+        $user = $userRepository->find($user->getId());
+        self::assertNotNull($user->getAccessCode());
+        self::assertNotNull($user->getCodeExpiration());
+        self::assertGreaterThan(new \DateTime(), $user->getCodeExpiration());
         self::assertResponseRedirects('/login/code');
     }
 }
