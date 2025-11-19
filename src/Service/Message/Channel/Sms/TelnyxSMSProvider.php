db9d8707b4400f670fd66a38c2f319170c66b7ce
Christian Baltazar <ing.chris@hotmail.com>
2025-11-19 07:52:37 +0100

    Create and wire client

---
+++ src/Service/Message/Channel/Sms/TelnyxSMSProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service\Message\Channel\Sms;
+
+use Telnyx\Client;
+
+class TelnyxSMSProvider implements SmsProviderInterface
+{
+
+    public function __construct(
+        private readonly Client $client,
+        private readonly string $messagingProfileId,
+    ) {}
+
+    public function send(string $number, string $sender, string $message): mixed
+    {
+        return $this->client->messages->send(
+            to: $number,
+            text: $message,
+//            messagingProfileID: $this->messagingProfileId,
+        );
+    }
+}
