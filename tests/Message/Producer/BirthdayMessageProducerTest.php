6f9340d7807d706310ae607778686bf10cb16232
Christian Baltazar <chrisbaltazar@users.noreply.github.com>
2025-11-22 13:28:01 +0100

    Fix/birthday producer hour (#16)

--- tests/Message/Producer/BirthdayMessageProducerTest.php
+++ tests/Message/Producer/BirthdayMessageProducerTest.php
@@ -46,13 +46,19 @@ class BirthdayMessageProducerTest extends AbstractKernelTestCase
         $test->produce();
 
         $this->assertDatabaseCount(3, Message::class);
+
+        $scheduledDate = (new \DateTimeImmutable('today'))->setTime(9, 0);
+        $this->assertSame(date('Ymd 09:00:00'), $scheduledDate->format('Ymd H:i:s'));
+
         $this->assertDatabaseEntity(Message::class, [
             'type' => MessageTypeEnum::USER_BIRTHDAY_GREET->value,
             'user' => $user1,
+            'scheduledAt' => $scheduledDate,
         ]);
         $this->assertDatabaseEntity(Message::class, [
             'type' => MessageTypeEnum::ADMIN_BIRTHDAY_NOTIF->value,
             'user' => $admin,
+            'scheduledAt' => $scheduledDate,
         ]);
     }
 
@@ -60,7 +66,7 @@ class BirthdayMessageProducerTest extends AbstractKernelTestCase
     {
         $message1 = MessageFactory::create(
             type: MessageTypeEnum::USER_BIRTHDAY_GREET,
-            scheduledAt: new \DateTimeImmutable('-1 min'),
+            scheduledAt: (new \DateTimeImmutable('today'))->setTime(9, 0),
         );
         $message1->setStatus(null);
         $message1->setProcessedAt(null);
@@ -81,13 +87,14 @@ class BirthdayMessageProducerTest extends AbstractKernelTestCase
 
         $message4 = MessageFactory::create(
             type: MessageTypeEnum::USER_BIRTHDAY_GREET,
-            scheduledAt: new \DateTimeImmutable('-1 min'),
+            scheduledAt: (new \DateTimeImmutable('today'))->setTime(9, 0),
         );
         $message4->setStatus(MessageStatusEnum::ERROR->value);
         $message4->setProcessedAt(new \DateTimeImmutable('now'));
 
         /** @var BirthdayMessageProducer $test */
         $test = $this->get(BirthdayMessageProducer::class);
+        $this->assertTrue($test->isRelevant($message1));
         $this->assertTrue($test->isWaiting($message1));
         $this->assertFalse($test->isWaiting($message2));
         $this->assertTrue($test->isRelevant($message3));
