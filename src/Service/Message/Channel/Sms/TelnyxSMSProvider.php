109cb1a2079e7b2f1381acb7878472a8b4ffe669
Christian Baltazar <ing.chris@hotmail.com>
2026-02-28 11:15:07 +0100

    fixup! USe retry client

--- src/Service/Message/Channel/Sms/TelnyxSMSProvider.php
+++ src/Service/Message/Channel/Sms/TelnyxSMSProvider.php
@@ -2,41 +2,36 @@
 
 namespace App\Service\Message\Channel\Sms;
 
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpClient\RetryableHttpClient;
-use Telnyx\Client;
 
 class TelnyxSMSProvider implements SmsProviderInterface
 {
 
     public function __construct(
         private readonly string $apiKey,
+        private readonly string $baseUrl,
         private readonly string $messagingProfileId,
         private readonly RetryableHttpClient $retryClient,
     ) {}
 
     public function send(string $number, string $sender, string $message): mixed
     {
-            return $this->retryClient->request(
-                method: 'POST',
-                url: 'https://api.telnyx.com/v2/messages',
-                options: [
-                    'json' => [
-                        'to' => $number,
-                        'text' => $message,
-                        'messaging_profile_id' => $this->messagingProfileId,
-                    ],
-                    'headers' => [
-                        'Authorization' => 'Bearer ' . $this->apiKey,
-                        'Content-Type' => 'application/json',
-                    ],
-                ]
-            );
+        $uri = sprintf('%s/%s', rtrim($this->baseUrl, '/'), 'messages');
 
-//        return $this->client->messages->send(
-//            to: $number,
-//            messagingProfileID: $this->messagingProfileId,
-//            text: $message,
-//        );
+        return $this->retryClient->request(
+            method: 'POST',
+            url: $uri,
+            options: [
+                'json' => [
+                    'to' => $number,
+                    'text' => $message,
+                    'messaging_profile_id' => $this->messagingProfileId,
+                ],
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->apiKey,
+                    'Content-Type' => 'application/json',
+                ],
+            ],
+        );
     }
 }
